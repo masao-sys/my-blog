91f2159612b9c9cfda6156fb632274b9420da4cf
Masao <gyohunori1985@gmail.com>
2024-09-27 18:36:14 +0900

    imageを試しに削除してみる

--- src/components/molecules/PostCard.tsx
+++ src/components/molecules/PostCard.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Post } from '../../types'
@@ -10,19 +10,7 @@ interface PostCardProps {
 const PostCard: React.FC<PostCardProps> = ({ post }) => {
   return (
     <div className="flex flex-col overflow-hidden rounded-lg border bg-white">
-      <Link
-        href={`/post/${post.id}`}
-        className="group relative block h-48 overflow-hidden bg-gray-100 md:h-64"
-      >
-        <Image
-          src={post.eyecatch.url}
-          width={post.eyecatch.width}
-          height={post.eyecatch.height}
-          alt={post.title}
-          loading="lazy"
-          className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
-        />
-      </Link>
+
 
       <div className="flex flex-1 flex-col p-4 sm:p-6">
         <h2 className="mb-2 text-lg font-semibold text-gray-800">
